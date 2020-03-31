2b7d1ed73136ef5a0d55d170a0b0a3cf3faad430
Kate Mangubat <kate.d.mangubat@gmail.com>
2020-03-30 22:24:40 -0700

    added function, reorganized code

--- calculator.js
+++ calculator.js
@@ -1,9 +1,12 @@
-const exampleInput = {
-    num1: 10,
-    num2: 5,
-    operation: 'tree',
+function addNumbers(aNum1, aNum2) {
+    return aNum1 + aNum2;
 }
 
+const exampleInput = {
+    num1: 10,
+    num2: 0,
+    operation: '/',
+}
 
 let validOperators = ['+', '-', '*', '/', 'add', 'subtract', 'multiply', 'divide'];
 let output = null;
@@ -11,23 +14,32 @@ let output = null;
 //check if num1 or num2 or operation is null
 if (exampleInput.num1 === null || exampleInput.num2 === null || exampleInput.operation === null) {
     console.log('Invalid input, input is null.');
-} else if (isNaN(exampleInput.num1) || isNaN(exampleInput.num2)) {
+    return;
+}
+
+if (isNaN(exampleInput.num1) || isNaN(exampleInput.num2)) {
     console.log('Invalid input, this is not a number.');
-} else if (!validOperators.includes(exampleInput.operation)) {
+    return;
+}
+
+if (!validOperators.includes(exampleInput.operation)) {
     console.log('Invalid operation');
-} else {
-    if ((exampleInput.operation === 'add') || (exampleInput.operation === '+')) {
-        // add num1 & num2
-        output = exampleInput.num1 + exampleInput.num2;
-    } else if ((exampleInput.operation === 'subtract') || (exampleInput.operation === '-')) {
-        // subtract num1 - num2
-        output = exampleInput.num1 - exampleInput.num2;
-    } else if ((exampleInput.operation === 'multiply') || (exampleInput.operation === '*')) {
-        // multiply num1 * num2
-        output = exampleInput.num1 * exampleInput.num2;
-    } else if ((exampleInput.operation === 'divide') || (exampleInput.operation === '/')) {
-        // divide num1 / num2
+    return;
+}
+
+// passed main checks
+// going to try to do an operation
+if ((exampleInput.operation === 'add') || (exampleInput.operation === '+')) {
+    output = addNumbers(exampleInput.num1, exampleInput.num2);
+} else if ((exampleInput.operation === 'subtract') || (exampleInput.operation === '-')) {
+    output = exampleInput.num1 - exampleInput.num2;
+} else if ((exampleInput.operation === 'multiply') || (exampleInput.operation === '*')) {
+    output = exampleInput.num1 * exampleInput.num2;
+} else if ((exampleInput.operation === 'divide') || (exampleInput.operation === '/')) {
+    if (exampleInput.num2 === 0) {
+        return console.log('Unable to divide by 0.')
+    } else {
         output = exampleInput.num1 / exampleInput.num2;
     }
-    console.log(output);
 }
+console.log(output);
